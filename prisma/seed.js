58ae2b71130364fcd518c4db80bc5d9ded1bb5c5
Robert Foppen <robert.foppen96@gmail.com>
2024-09-30 10:36:55 +0200

    create seed.js

---
+++ prisma/seed.js
@@ -0,0 +1,81 @@
+import { PrismaClient } from "@prisma/client";
+import amenityData from "../src/data/amenities.json" assert { type: "json" };
+import bookingData from "../src/data/bookings.json" assert { type: "json" };
+import hostData from "../src/data/hosts.json" assert { type: "json" };
+import propertyData from "../src/data/properties.json" assert { type: "json" };
+import reviewData from "../src/data/reviews.json" assert { type: "json" };
+import userData from "../src/data/users.json" assert { type: "json" };
+
+const prisma = new PrismaClient();
+
+async function main() {
+  // Create hosts
+  const { hosts } = hostData;
+  for (const host of hosts) {
+    await prisma.host.upsert({
+      where: { id: host.id },
+      update: {},
+      create: host,
+    });
+  }
+
+  // Create properties
+  const { properties } = propertyData;
+  for (const property of properties) {
+    await prisma.property.upsert({
+      where: { id: property.id },
+      update: {},
+      create: property,
+    });
+  }
+
+  // Create users
+  const { users } = userData;
+  for (const user of users) {
+    await prisma.user.upsert({
+      where: { id: user.id },
+      update: {},
+      create: user,
+    });
+  }
+
+  // Create amenities
+  const { amenities } = amenityData;
+  for (const amenity of amenities) {
+    await prisma.amenity.upsert({
+      where: { id: amenity.id },
+      update: {},
+      create: amenity,
+    });
+  }
+
+  // Create bookings
+  const { bookings } = bookingData;
+  for (const booking of bookings) {
+    await prisma.booking.upsert({
+      where: { id: booking.id },
+      update: {},
+      create: booking,
+    });
+  }
+
+  // Create reviews
+  const { reviews } = reviewData;
+  for (const review of reviews) {
+    await prisma.review.upsert({
+      where: { id: review.id },
+      update: {},
+      create: review,
+    });
+  }
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
