f35fd2051307dba588073d2eb99b72e5a876741a
Robert Foppen <robert.foppen96@gmail.com>
2024-10-03 10:17:53 +0200

    Change error message, adjust schema for m-to-m relationships

--- src/middleware/errorMiddleware.js
+++ src/middleware/errorMiddleware.js
@@ -1,7 +1,8 @@
 const errorHandler = (err, req, res, next) => {
   console.error(err);
   res.status(500).json({
-    message: "Something went wrong. Check your request and try again.",
+    message:
+      "An error occurred on the server, please double-check your request!",
   });
 };
 
